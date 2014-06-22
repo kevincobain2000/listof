687b194520aa6647e2865af7d3560d17466dfac5
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2014-06-22 13:57:32 +0900

    DATA

--- test/listof.rb
+++ test/listof.rb
@@ -1,9 +1,10 @@
-# gem build listof.gemspec ;gem install listof;ruby test/listof.rb
+# ait add .;gem build listof.gemspec ;gem install listof;ruby test/listof.rb
 require 'listof'
 
 # puts Listof.find("countries")
 # puts Listof.find("countries").to_json
 # puts Listof.find("occupations").to_yaml
 # puts Listof.find("indian_companies").to_yaml
+puts Listof.find("kanji_three_strokes").to_yaml
 # puts Listof.by_alphabet("c")
-puts Listof.all().to_yaml
+# puts Listof.all().to_yaml
