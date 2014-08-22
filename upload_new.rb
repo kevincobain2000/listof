2c0d8498256669ae08f4d87951fb57274affc965
Pulkit Kathuria <kevincobain2000@gmail.com>
2014-08-22 17:58:01 +0900

    load data from json

---
+++ upload_new.rb
@@ -0,0 +1,11 @@
+require 'net/http'
+require 'json'
+
+source = "http://listof.herokuapp.com/submit/all_submitted.json"
+resp = Net::HTTP.get_response(URI.parse(source))
+data = resp.body
+result = JSON.parse(data)
+
+result.each do |title, list|
+  File.open("data/#{title}.txt", 'w') { |file| file.write(list.join("\n")) }
+end
