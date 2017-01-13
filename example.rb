73d6e2414951311c37138bb84ebe9d9c0d63b79c
Pulkit Kathuria <kevincobain2000@gmail.com>
2017-01-13 13:56:09 +0900

    added example

---
+++ example.rb
@@ -0,0 +1,7 @@
+require 'listof'
+
+# puts Listof.find("countries")
+# puts Listof.find("countries").to_json
+puts Listof.find("occupations").to_yaml
+# puts Listof.find("top_movies").to_yaml
+# puts Listof.all().to_yaml
