9e2a4edd0e4fc6740580b48bb9407dc293b31a8e
Pulkit Kathuria <kevincobain2000@gmail.com>
2017-01-13 13:59:18 +0900

    update Glossary

--- example.rb
+++ example.rb
@@ -2,6 +2,6 @@ require 'listof'
 
 # puts Listof.find("countries")
 # puts Listof.find("countries").to_json
-puts Listof.find("occupations").to_yaml
+# puts Listof.find("occupations").to_yaml
 # puts Listof.find("top_movies").to_yaml
-# puts Listof.all().to_yaml
+puts Listof.all().to_yaml
