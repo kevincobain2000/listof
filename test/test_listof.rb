6443a1802796d0b09f29f4d500b600640b657f0d
Pulkit Kathuria <kevincobain2000@gmail.com>
2017-02-05 13:00:15 +0900

    gen links

--- test/test_listof.rb
+++ test/test_listof.rb
@@ -5,11 +5,23 @@ require 'minitest/autorun'
 # puts Listof.find("countries").to_json
 # puts Listof.find("occupations").to_yaml
 
-puts Listof.all().to_yaml
+# puts Listof.all().to_yaml
 # puts Listof.find("alphabets").to_yaml
 # alphabets = Listof.find("alphabets")
 # puts alphabets['source']
 # puts alphabets['image']
+
+
+
+all = Listof.all()
+git_file_path = "https://github.com/kevincobain2000/listof/blob/master/data/"
+all.each do |a|
+    list_title = a[1]
+    list_link = git_file_path + list_title + '.txt'
+
+    md_link = "[" + list_title + "](" + list_link + ")"
+    puts "- " + md_link
+end
 
 # Test on all data
 class ListofTest < MiniTest::Unit::TestCase
@@ -17,10 +29,10 @@ class ListofTest < MiniTest::Unit::TestCase
        assert_equal "Hash", Listof.all().class.to_s
     end
     def test_each_class
-    	all = Listof.all()
-    	all.each do |a|
-    		list_title = a[1]
-    		assert_equal "Hash", Listof.find(list_title.to_s).class.to_s
-    	end
+        all = Listof.all()
+        all.each do |a|
+            list_title = a[1]
+            assert_equal "Hash", Listof.find(list_title.to_s).class.to_s
+        end
     end
 end
