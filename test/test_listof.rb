ecf8fc15a8c888bc04fd7c1b55f9d29660cc4b4b
Pulkit Kathuria <kevincobain2000@gmail.com>
2017-02-06 11:38:21 +0900

    unwanted code

--- test/test_listof.rb
+++ test/test_listof.rb
@@ -6,22 +6,7 @@ require 'minitest/autorun'
 # puts Listof.find("occupations").to_yaml
 
 # puts Listof.all().to_yaml
-# puts Listof.find("alphabets").to_yaml
-# alphabets = Listof.find("alphabets")
-# puts alphabets['source']
-# puts alphabets['image']
 
-
-
-all = Listof.all()
-git_file_path = "https://github.com/kevincobain2000/listof/blob/master/data/"
-all.each do |a|
-    list_title = a[1]
-    list_link = git_file_path + list_title + '.txt'
-
-    md_link = "[" + list_title + "](" + list_link + ")"
-    puts "- " + md_link
-end
 
 # Test on all data
 class ListofTest < MiniTest::Unit::TestCase
